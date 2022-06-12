1720856f74849e46094c5fee27e80b8344643d74
SzantoGaborCC <szanto.gabor0204@gmail.com>
2022-06-12 23:43:06 +0200

    Gun controller and webpages updated.

--- src/main/java/com/codecool/navymanager/controller/mvc/GunController.java
+++ src/main/java/com/codecool/navymanager/controller/mvc/GunController.java
@@ -1,90 +1,99 @@
 package com.codecool.navymanager.controller.mvc;
 
-
 import com.codecool.navymanager.entityDTO.CountryDto;
+import com.codecool.navymanager.entityDTO.GunDto;
 import com.codecool.navymanager.response.Response;
 import com.codecool.navymanager.service.CountryService;
+import com.codecool.navymanager.service.GunService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/country")
-public class CountryController {
+@RequestMapping("/gun")
+public class GunController {
+    private final GunService gunService;
     private final CountryService countryService;
 
-    public CountryController(CountryService countryService) {
+    public GunController(GunService gunService, CountryService countryService) {
+        this.gunService = gunService;
         this.countryService = countryService;
     }
 
     @GetMapping
-    public String listCountries(Model model) {
-        model.addAttribute("countries", countryService.findAll());
-        return "country-list";
+    public String listGuns(Model model) {
+        model.addAttribute("guns", gunService.findAll());
+        return "gun-list";
     }
 
-    @GetMapping("/{id}")
     public String showDetails(@PathVariable Long id, Model model) {
-        CountryDto country = countryService.findById(id);
-        model.addAttribute("country", country);
-        return "country-details";
+        GunDto gun = gunService.findById(id);
+        model.addAttribute("gun", gun);
+        return "gun-details";
     }
 
     @GetMapping("/show-add-form")
     public String showAddForm(Model model){
         model.addAttribute("add", true);
-        model.addAttribute("country", new CountryDto());
-        return "country-form";
+        model.addAttribute("gun", new GunDto());
+        model.addAttribute("validCountryValues", countryService.findAll());
+        return "gun-form";
     }
 
-    @PostMapping()
-    public ResponseEntity<Response> add(@ModelAttribute("country") @Valid CountryDto country, BindingResult result, Model model) {
+    @PostMapping
+    public ResponseEntity<Response> add(@ModelAttribute("gun") @Valid GunDto gun, BindingResult result, Model model) {
         Response response = new Response();
         if (result.hasErrors()) {
             model.addAttribute("add", true);
+            model.addAttribute("validCountryValues", countryService.findAll());
             response.setErrorMessages(result.getFieldErrors().stream()
                     .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
-            response.setMessage("Invalid country data!");
+            response.setMessage("Invalid gun data!");
             return ResponseEntity.badRequest().body(response);
         }
-        countryService.add(country);
-        response.setMessage("Country was added.");
+        gunService.add(gun);
+        response.setMessage("Gun was added.");
         return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteById(@PathVariable Long id) {
-        countryService.deleteById(id);
-        return ResponseEntity.ok().body("Country was removed.");
+        gunService.deleteById(id);
+        return ResponseEntity.ok().body("Gun was removed.");
     }
 
     @GetMapping("/{id}/show-update-form")
     public String showUpdateForm(@PathVariable Long id, Model model) {
-            CountryDto country = countryService.findById(id);
+            GunDto gun = gunService.findById(id);
             model.addAttribute("add", false);
-            model.addAttribute("country", country);
-            return "country-form";
+            model.addAttribute("gun", gun);
+            model.addAttribute("validCountryValues", countryService.findAll());
+            return "gun-form";
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Response> update(@PathVariable long id, @ModelAttribute("country") @Valid CountryDto country, BindingResult result, Model model) {
+    public ResponseEntity<Response> update(@PathVariable long id, @ModelAttribute("gun") @Valid GunDto gun, BindingResult result, Model model) {
         Response response = new Response();
         if (result.hasErrors()) {
             model.addAttribute("add", false);
+            model.addAttribute("validCountryValues", countryService.findAll());
             response.setErrorMessages(result.getFieldErrors().stream()
                     .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
-            response.setMessage("Invalid country data!");
+            response.setMessage("Invalid gun data!");
             return ResponseEntity.badRequest().body(response);
         }
-        countryService.update(country, id);
-        response.setMessage("Country was updated.");
+        gunService.update(gun, id);
+        response.setMessage("Gun was updated.");
         return ResponseEntity.ok().body(response);
     }
+    //todo: gun controller and webpages were updated, tomorrow test them.
 }
 
